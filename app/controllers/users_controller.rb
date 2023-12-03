b55938a2f3838f9b5053eefd7adb57cb9ec33fbf
carlodimauro <146589712+carlodimauro@users.noreply.github.com>
2023-12-03 23:39:13 +0000

    added pages for feed and discover

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,23 @@ class UsersController < ApplicationController
     render({ :template => "users/show" })
   end
 
+def liked_photos
+  the_username = params.fetch("username")
+  @the_user = User.where({ :username => the_username }).first
+  render({ :template => "users/liked_photos" })
+end
+
+def feed
+  the_username = params.fetch("username")
+  @the_user = User.where({ :username => the_username }).first
+  render({ :template => "users/feed" })
+end
+
+def discover
+  the_username = params.fetch("username")
+  @the_user = User.where({ :username => the_username }).first
+  render({ :template => "users/discover" })
+end
+
+
 end
